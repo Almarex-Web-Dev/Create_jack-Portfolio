5af6fdc9108a681ccb8778149fc6c0722de63455
Almarex <almarexwebdev@gmail.com>
2022-01-21 12:31:39 +0100

    Javascript files

---
+++ index.js
@@ -0,0 +1,46 @@
+const soundCloud = document.querySelector('.sound-cloud');
+const off = document.querySelector('#off');
+const on = document.querySelector('#on');
+const myAudio = document.querySelector('#myAudio');
+
+off.addEventListener('click', () => soundTrack('off'));
+on.addEventListener('click', () => soundTrack('on'));
+
+const soundTrack = (soundState) => {
+    if(soundState === 'off'){
+        on.style.display = 'block';
+        off.style.display = 'none';
+        soundCloud.style.color = "#08fdd8";
+        myAudio.play();
+    }
+    else if(soundState === 'on'){
+        on.style.display = 'none';
+        off.style.display = 'block';
+        soundCloud.style.color = "#f50057";
+        myAudio.pause();
+    }
+}
+
+// Play music functionality
+
+const btnBars = document.querySelector('.bars');
+const btnTimes = document.querySelector('.times');
+const SideNav = document.querySelector('.aside');
+
+
+btnBars.addEventListener('click', () => myFunc('open'));
+btnTimes.addEventListener('click', () => myFunc('close'));
+
+const myFunc = (navCondition) => {
+    if(navCondition === 'open'){
+        SideNav.classList.add('show-nav');
+        btnTimes.style.display = "block";
+        btnBars.style.display = "none";
+    }
+    else if(navCondition === 'close'){
+            SideNav.classList.remove('show-nav');
+            btnTimes.style.display = "none";
+            btnBars.style.display = "block";
+    }
+}
+
